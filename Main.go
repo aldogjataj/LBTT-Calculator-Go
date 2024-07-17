81464cd9b72ba8c968c31a72aa76abdc75128d7e
Aldo Gjataj <aldo.gjataj@bjss.com>
2024-07-17 17:03:57 +0100

    post pair programming

--- Main.go
+++ Main.go
@@ -1,20 +1,43 @@
 package main
 
-func CalculateLBTT(price float64) float64 {
+import (
+	"errors"
+)
+
+type TaxBracket struct {
+	MinimumPrice  float64
+	MaximumPrice  float64
+	TaxMultiplier float64
+}
+
+var TaxBrackets []TaxBracket = []TaxBracket{
+	TaxBracket{0.0, 145000, 0.0},
+	TaxBracket{145000, 250000, 0.02},
+	TaxBracket{250000, 325000, 0.05},
+	TaxBracket{325000, 750000, 0.1},
+	TaxBracket{750000, 99999999, 0.12},
+}
+
+func CalculateLBTT(price float64) (float64, error) {
 	var tax float64
 
-	if price <= 145000 {
-		tax = 0
-	} else if price <= 250000 {
-		tax = (price - 145000) * 0.02
-	} else if price <= 325000 {
-		tax = (price-250000)*0.05 + 2100
-	} else if price <= 750000 {
-		tax = (price-325000)*0.10 + 5850
-	} else {
-		tax = (price-750000)*0.12 + 48350
+	if price < 0 {
+		return 0, errors.New("negative number")
 	}
-	//Numbers being added are the amount taxed by default when the amount is above a threshold
 
-	return tax
+	for i := 0; i < len(TaxBrackets); i++ {
+		bracket := TaxBrackets[i]
+		if price > bracket.MinimumPrice {
+			if price <= bracket.MaximumPrice {
+				TaxForBand := (price - bracket.MinimumPrice) * bracket.TaxMultiplier
+				tax += TaxForBand
+			} else {
+				TaxForBand := (bracket.MaximumPrice - bracket.MinimumPrice) * bracket.TaxMultiplier
+				tax += TaxForBand
+			}
+		}
+
+	}
+
+	return tax, nil
 }
--- Main_test.go
+++ Main_test.go
@@ -5,37 +5,47 @@ import (
 )
 
 func TestCalculateLBTTForHouseBelow145K(t *testing.T) {
-	got := CalculateLBTT(144999)
+	got, _ := CalculateLBTT(144999)
 	want := 0.0
 	if got != want {
 		t.Error("Expected no tax, got ", got)
 	}
 }
 func TestCalculateLBTTForHouseWithin145To250KBracket(t *testing.T) {
-	got := CalculateLBTT(200000)
+	got, _ := CalculateLBTT(200000)
 	want := 1100.0
 	if got != want {
 		t.Error("Expected £1100 tax, got ", got)
 	}
 }
 func TestCalculateLBTTForHouseWithin250To325KBracket(t *testing.T) {
-	got := CalculateLBTT(300000)
+	got, _ := CalculateLBTT(300000)
 	want := 4600.0
 	if got != want {
 		t.Error("Expected £4600 tax, got ", got)
 	}
 }
 func TestCalculateLBTTForHouseWithin325kTo750KBracket(t *testing.T) {
-	got := CalculateLBTT(550000.0)
+	got, _ := CalculateLBTT(550000.0)
 	want := 28350.0
 	if got != want {
 		t.Error("Expected £28350 tax, got ", got)
 	}
 }
 func TestCalculateLBTTForHouseInTheOver750KBracket(t *testing.T) {
-	got := CalculateLBTT(1000000.0)
+	got, _ := CalculateLBTT(1000000.0)
 	want := 78350.0
 	if got != want {
 		t.Error("Expected £78350 tax, got ", got)
+	}
+}
+func TestNegativePriceWouldBeInvalid(t *testing.T) {
+	got, err := CalculateLBTT(-1000)
+	want := 0.0
+	if err == nil {
+		t.Error("Expected an error, did not get one.")
+	}
+	if got != want {
+		t.Error("Expected no tax since the number is invalid, got a number for the tax.")
 	}
 }
